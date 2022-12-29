820f805e683d9552a24c9d50369e747c38f5c332
Cooper Harasyn <charasyn@outlook.com>
2022-12-29 00:10:14 -0800

    Remove copy of Bowspr.ccs after building example

--- example/make.bat
+++ example/make.bat
@@ -2,4 +2,5 @@
 copy build\clean-expanded.sfc build\patched.sfc > NUL
 copy ..\Bowspr.ccs Bowspr.ccs > NUL
 ..\..\ccscript13\ccc.exe --summary summary.txt --no-reset --start f00000 --end f0ffff -o build\patched.sfc example.ccs
+del Bowspr.ccs > NUL
 echo Done patching.
