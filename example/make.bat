367885a7e278c90830fe05a0bfcd40d5ffe0ab8d
Cooper Harasyn <charasyn@outlook.com>
2024-01-31 19:18:56 -0800

    Update to v0.71
    See changelog in Bowspr.ccs for more info

--- example/make.bat
+++ example/make.bat
@@ -1,6 +1,8 @@
 @echo off
 copy build\clean-expanded.sfc build\patched.sfc > NUL
 copy ..\Bowspr.ccs Bowspr.ccs > NUL
+copy ..\movscr_codes.ccs movscr_codes.ccs > NUL
 ..\..\ccscript13\ccc.exe --summary summary.txt --no-reset --start f00000 --end f0ffff -o build\patched.sfc example.ccs
 del Bowspr.ccs > NUL
+del movscr_codes.ccs > NUL
 echo Done patching.
